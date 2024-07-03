d77cbf157b321031d658daef10363de5582981d0
shegzyrey <rey@DESKTOP-P49K6KF.localdomain>
2024-07-02 21:41:29 -0700

    custom index

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,14 +4,14 @@ package { 'nginx':
     ensure => 'installed'
 }
 
-file { '/var/www/html/index.html':
+file { '/var/www/html/index.nginx-debian.html':
     content => 'Hello World',
 }
 
 file_line { 'redirection-301':
     ensure => 'present'
     path   => '/etc/nginx/sites-available/default'
-    after  => 'listen 80 default_server;'
+    after  => 'server_name_;'
     line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
 }
 
